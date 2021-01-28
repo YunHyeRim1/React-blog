1272617dc00a477c3ba6b8427c087035c8f2cd3b
YunHyeRim1 <0305_08@naver.com>
2021-01-28 17:12:01 +0900

    17

---
+++ src/cmm/pages/index.js
@@ -0,0 +1,9 @@
+export { MainPage } from './MainPage'
+export { Blog } from './Blog'
+export { FeaturedPost } from './BloFeaturedPostg'
+export { Footer } from './Footer'
+export { Header } from './Header'
+export { MainFeaturedPost } from './MainFeaturedPost'
+export { Markdown } from './Markdown'
+export { Sidebar } from './Sidebar'
+
